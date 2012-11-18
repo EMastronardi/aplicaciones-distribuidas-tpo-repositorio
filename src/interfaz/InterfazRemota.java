d839bfdc6ba6a843a296bd5912bf52885272fd1d
emastronardi <emastronardi@gmail.com>
2012-11-18 23:16:06 +0000

    Primer commit del Repositorio

---
+++ src/interfaz/InterfazRemota.java
@@ -0,0 +1,11 @@
+package interfaz;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface InterfazRemota extends Remote{
+
+	//public void metodoPublicado() throws RemoteException ;
+
+}
